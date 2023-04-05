5159965cc9c6b4e7b5f266a52de32bb4abc64066
Zebulon Goriely <zepsilon32@gmail.com>
2023-04-05 17:50:10 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 if [ ! -d "env" ]; then
-	python3.8 -m venv "env"
+	module load python/3.8
+    virtualenv -p python3.8 env
 	source env/bin/activate
 	pip install -r requirements.txt
 	pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
