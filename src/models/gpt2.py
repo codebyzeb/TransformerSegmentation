cd21df2f5bc1258fc56bc9801375b82af5b99d3f
Zeb <zepsilon32@gmail.com>
2023-06-23 15:31:06 +0100

    Use multi-hot encoding for features

--- src/models/gpt2.py
+++ src/models/gpt2.py
@@ -3,24 +3,19 @@ from typing import Optional, Tuple, Union
 import torch
 import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss, Linear, LayerNorm, ModuleList, Sequential, Module
-from transformers.modeling_outputs import TokenClassifierOutput
+from transformers import GPT2Config, GPT2ForTokenClassification, GPT2LMHeadModel, GPT2Model, GPT2PreTrainedModel
+from transformers.modeling_outputs import TokenClassifierOutput, CausalLMOutputWithCrossAttentions
 from torch.nn.parameter import Parameter
 
 from .registry import register_model
+from ..preprocessing import FEATURES
 
 """ 
 Model classes use the GPT-2 Architecture 
 """
 
-from transformers import (
-    GPT2Config,
-    GPT2ForTokenClassification,
-    GPT2LMHeadModel,
-    GPT2PreTrainedModel,
-    GPT2Model,
-)
 
-from transformers.modeling_outputs import CausalLMOutputWithCrossAttentions
+FEATURE_VEC_SIZE = len(FEATURES) + 1
 
 ### Wrapping the GPT2 models to make them compatible with the model registry ###
 
@@ -31,9 +26,6 @@ class GPT2LMHeadModel(GPT2LMHeadModel):
 
 
 ### Custom GPT2 Models ###
-
-# NOTE: The forward pass of these models always needs to return ModelOutput
-#       objects. See the documentation for more details.
 
 
 @register_model("tuned_gpt2_lm_head_model", GPT2Config)
@@ -118,18 +110,39 @@ class CustomLinear(Linear):
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return F.linear(input, self.weight.T, self.bias)
 
+
 class FeatureMap(Module):
     def __init__(self, feature_map, device=None, dtype=torch.float32):
         super().__init__()
         factory_kwargs = {"device": device, "dtype": dtype}
-        self.weight = torch.nn.Parameter(torch.tensor([feature_map[i] for i in range(len(feature_map))], **factory_kwargs), requires_grad=False)
-        
+        # The actual feature map is a multi-hot vector of size 3 * len(FEATURES)
+        # So each group of three elements is a feature
+        multi_hot_feature_map = []
+        for i in range(len(feature_map)):
+            feature_vec = []
+            for j in range(len(feature_map[i])):
+                feature_vec.extend([1, 0, 0] if feature_map[i][j] == 0 else [0, 1, 0] if feature_map[i][j] == 1 else [0, 0, 1])
+            multi_hot_feature_map.append(feature_vec)
+
+        self.weight_as_indices = torch.nn.Parameter(
+            torch.tensor([feature_map[i] for i in range(len(feature_map))], **factory_kwargs), requires_grad=False
+        )
+        self.weight = torch.nn.Parameter(torch.tensor(multi_hot_feature_map, **factory_kwargs), requires_grad=False)
+
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         ignore = input == -100
         input[ignore] = 0
         output = F.embedding(input, self.weight)
-        output[ignore] = torch.zeros(self.weight.shape[-1]) - 100
+        output[ignore] = torch.zeros(self.weight.shape[-1], device=input.device) - 100
         return output
+
+    def as_indices(self, input: torch.Tensor) -> torch.Tensor:
+        ignore = input == -100
+        input[ignore] = 0
+        output = F.embedding(input, self.weight_as_indices)
+        output[ignore] = torch.zeros(self.weight_as_indices.shape[-1], device=input.device) - 100
+        return output
+
 
 @register_model("gpt2_feature_model", GPT2Config)
 class GPT2FeatureModel(GPT2PreTrainedModel):
@@ -156,6 +169,7 @@ class GPT2FeatureModel(GPT2PreTrainedModel):
         """
         super().__init__(config)
 
+        # Save vocab size for token generation
         self.vocab_size = config.vocab_size
 
         # Used to flip the output logits of forward() back to vocab indices for the purpose of generation
@@ -164,28 +178,45 @@ class GPT2FeatureModel(GPT2PreTrainedModel):
         # Feature map should be provided when initialising, but when reloading from checkpoint
         # we need to set it to None and it'll be initialised from the checkpoint
         if feature_map is None:
-            feature_map = {i: [0]*39 for i in range(config.vocab_size)}
-        
+            feature_map = {i: [0] * 39 for i in range(config.vocab_size)}
+
         # Feature Map layer - maps from token ids to feature vectors. Doesn't update during training.
         self.feature_map = FeatureMap(feature_map)
         self.feature_size = self.feature_map.weight.shape[1]
 
         # Embedding layer - map from feature vector to embedding with layer norm
-        self.custom_embedding = Sequential(
-            CustomLinear(self.feature_size, config.n_embd),
-            LayerNorm(config.n_embd, eps=config.layer_norm_epsilon)
-        )
+        self.custom_embedding = CustomLinear(self.feature_size, config.n_embd)
+        self.embedding_norm = LayerNorm(config.n_embd, eps=config.layer_norm_epsilon)
 
-        # Transformer unchanged
+        # Transformer unchanged – note that the embedding layer is now custom_embedding
         self.transformer = GPT2Model(config)
 
-        # Instead of mapping to vocab, we have a prediction head for each feature
-        self.lm_heads = ModuleList([Linear(config.n_embd, 3, bias=False) for _ in range(self.feature_size)])
+        # LM Head is a linear layer that maps from the transformer hidden state to the feature vector
+        self.lm_head = Linear(config.n_embd, self.feature_size, bias=False)
 
         self.model_parallel = False
         self.device_map = None
+
+        # Initialize weights and apply final processing
         self.post_init()
 
+    # Override from GPT2Model so that our custom embedding layer is tied to the output layer
+    def get_input_embeddings(self):
+        return self.custom_embedding
+
+    # Override from GPT2Model so that our custom embedding layer is tied to the output layer
+    def set_input_embeddings(self, new_embeddings):
+        self.custom_embedding = new_embeddings
+
+    # Copied from transformers.models.gpt2.modeling_gpt2.GPT2LMHeadModel so that we can tie the output layer to the embedding layer
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    # Copied from transformers.models.gpt2.modeling_gpt2.GPT2LMHeadModel so that we can tie the output layer to the embedding layer
+    def set_output_embeddings(self, new_embeddings):
+        self.lm_head = new_embeddings
+
+    # Copied from transformers.models.gpt2.modeling_gpt2.GPT2LMHeadModel
     def prepare_inputs_for_generation(self, input_ids, past_key_values=None, inputs_embeds=None, **kwargs):
         token_type_ids = kwargs.get("token_type_ids", None)
         # only last token for inputs_ids if past is defined in kwargs
@@ -248,10 +279,11 @@ class GPT2FeatureModel(GPT2PreTrainedModel):
         """
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        # ADDED BEHAVIOUR - Instead of passing input_ids to transformer, we map to feature vector
+        # ADDED BEHAVIOUR - Instead of passing input_ids to transformer, we first map to feature vector
         if inputs_embeds is None and input_ids is not None:
             inputs_embeds = self.feature_map(input_ids)
             inputs_embeds = self.custom_embedding(inputs_embeds)
+            inputs_embeds = self.embedding_norm(inputs_embeds)
             # If less than 2 dimensions, unsqueeze
             if len(inputs_embeds.shape) < 3:
                 inputs_embeds = inputs_embeds.unsqueeze(dim=0)
@@ -279,35 +311,45 @@ class GPT2FeatureModel(GPT2PreTrainedModel):
             torch.cuda.set_device(self.transformer.first_device)
             hidden_states = hidden_states.to(self.lm_heads[0].weight.device)
 
-        # ADDED BEHAVIOUR - multiple lm head predict features, instead a single lm head to predict a token
-        feature_logits = [lm_head(hidden_states) for lm_head in self.lm_heads]
+        lm_logits = self.lm_head(hidden_states)
+
+        # Split logits into groups of 3, one for each feature
+        # Lots of subtley here to deal with the fact that features are multi-hot encoded
+        # and we need to predict each feature independently
+        shape = list(lm_logits.shape)
+        shape[-1] //= 3
+        lm_logits = lm_logits.view(*shape, 3)
 
         loss = None
         if labels is not None:
-            loss = 0
-            label_vectors = self.feature_map(labels)
-            for i, logits in enumerate(feature_logits):
-                # Shift so that tokens < n predict n
-                shift_logits = logits[..., :-1, :].contiguous()
-                shift_labels = label_vectors[..., 1:, i].contiguous().long()
+            # Get label vectors as indices rather than one-hot vectors
+            label_vectors = self.feature_map.as_indices(labels)
+            # Shift so that tokens < n predict n
+            shift_logits = lm_logits[..., :-1, :, :].contiguous()
+            shift_labels = label_vectors[..., 1:, :].contiguous().long()
 
-                # Flatten the tokens
-                loss_fct = CrossEntropyLoss()
-                loss += loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)) / self.feature_size
+            # Flatten the tokens, ensure we split final dimension into groups of 3
+            # since every group of 3 represents a feature
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
 
         # Generate a tensor for token ids that has the same shape as logits
         if self.return_token_logits:
-            shape = list(feature_logits[0].shape)
+            shape = list(lm_logits.shape)[:-1]
             shape[-1] = self.vocab_size
-            log_probs = torch.zeros(shape, device=feature_logits[0].device)
-            feature_matrix_long = self.feature_map.weight.long()
-            for feature in range(self.feature_size):
-                feature_probs = F.log_softmax(feature_logits[feature], dim=-1)
-                log_probs += feature_probs[:, :, feature_matrix_long[:, feature]]
-            log_probs += math.log(1 / self.feature_size)
-            logits = log_probs - torch.log(1 - torch.exp(log_probs))  # Convert back to logits
+            log_probs = torch.zeros(shape, device=lm_logits.device)
+            feature_matrix_long = self.feature_map.weight_as_indices.long()
+            # To get the probablility of each feature, we softmax over the logits for each feature
+            for feature in range(self.feature_size // 3):
+                feature_probs = F.log_softmax(lm_logits[..., feature, :], dim=-1)
+                log_probs += feature_probs[..., feature_matrix_long[..., feature]]
+
+            # Normalise log probabilities
+            log_probs -= torch.logsumexp(log_probs, dim=-1, keepdim=True)
+            # Convert back to logits
+            logits = log_probs - torch.log(1 - torch.exp(log_probs))
         else:
-            logits = torch.stack(feature_logits, dim=1)
+            logits = lm_logits
 
         if not return_dict:
             output = (logits,) + transformer_outputs[1:]
