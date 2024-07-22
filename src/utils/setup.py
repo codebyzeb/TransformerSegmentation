40e911915aa3881948d3415201a9d01a2112d488
Zeb <zepsilon32@gmail.com>
2024-07-22 13:18:21 +0100

    Refactor where setup functions live:

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -1,6 +1,5 @@
-""" Utilities for setting up experiments"""
+""" Utilities for setting up experiments from config """
 
-from typing import Optional
 import datasets
 import logging
 import numpy as np
@@ -9,14 +8,22 @@ import os
 import torch
 import wandb
 
-from src import config
+from transformers import PreTrainedTokenizer, AutoTokenizer, PreTrainedTokenizerFast
+from typing import Optional, Union
 from omegaconf import OmegaConf
+
+from src import config
 
 # A logger for this file
 logger = logging.getLogger(__name__)
 
 def set_seed(seed: int) -> None:
-    """Sets seed for reproducibility"""
+    """Sets seed for reproducibility
+    
+    Args:
+        seed (int): seed to set
+    """
+
     if seed < 0:
         logger.warning("Skipping seed setting for reproducibility")
         logger.warning(
@@ -30,80 +37,15 @@ def set_seed(seed: int) -> None:
     if torch.cuda.is_available() > 0:
         torch.cuda.manual_seed_all(seed)
 
-def check_and_set_environment_variables(cfg: config.TransformerSegmentationConfig) -> None:
-    """ Checks huggingface tokens exist and sets up wandb environment variables """
-
-    assert (
-        "HF_READ_TOKEN" in os.environ and "HF_WRITE_TOKEN" in os.environ
-    ), "HF_READ_TOKEN and HF_WRITE_TOKEN need to be set as environment variables. Check .env file and source it if necessary."
-
-    if cfg.experiment.offline_run:
-        os.environ["WANDB_DISABLED"] = "true"
-        os.environ["WANDB_MODE"] = "disabled"
-        if cfg.experiment.resume_run_id is not None:
-            raise RuntimeError("resume_run_id is set but offline_run is True. Ignoring resume_run_id.")
-    else:
-        assert (
-            "WANDB_ENTITY" in os.environ
-        ), "WANDB_ENTITY needs to be set as an environment variable if not running in offline mode. Check .env file and source it if necessary."
-        if cfg.experiment.resume_checkpoint_path and cfg.experiment.resume_run_id is None:
-            raise RuntimeError("resume_run_id must be set if resume_checkpoint_path is set")
-
-     # Disable parallelism in tokenizers to avoid issues with multiprocessing
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-def check_config(cfg: config.TransformerSegmentationConfig) -> None:
-    """ Infer missing config values (checkpoint path and/or experiment name) and check if keys are missing """
-
-    # It is possible to infer the name if resume_run_id is set or if resume_checkpoint_path is set. Otherwise, a random name is generated.
-    if cfg.experiment.resume_run_id:
-        # Case when resume_run_id provided but not the experiment name
-        wandb_entity = os.environ.get("WANDB_ENTITY")
-        if "name" not in cfg.experiment:
-            api = wandb.Api()
-            run = api.run(f"{wandb_entity}/{cfg.experiment.group}/{cfg.experiment.resume_run_id}")
-            cfg.experiment.name = run.name
-            logger.info(f"experiment.name not set, loaded {cfg.experiment.name} from resume_run_id {cfg.experiment.resume_run_id} on wandb.")
-        # Case when resume_run_id provided but not the checkpoint path
-        if not cfg.experiment.resume_checkpoint_path:
-            checkpoint_paths = [dir for dir in os.listdir(f"checkpoints/{cfg.experiment.group}/{cfg.experiment.name}") if dir.startswith("checkpoint")]
-            if len(checkpoint_paths) > 0:
-                checkpoint_numbers = [int(path.split("-")[-1]) for path in checkpoint_paths]
-                checkpoint_numbers.sort()
-                cfg.experiment.resume_checkpoint_path = f"checkpoints/{cfg.experiment.group}/{cfg.experiment.name}/checkpoint-{checkpoint_numbers[-1]}"
-                logger.info(f"resume_checkpoint_path not set, loaded {cfg.experiment.resume_checkpoint_path} from latest checkpoint.")
-            else:
-                raise RuntimeError(f"resume_run_id set but no checkpoints found in the run directory checkpoints/{cfg.experiment.group}/{cfg.experiment.name}. Please specify resume_checkpoint_path.")
-    if "name" not in cfg.experiment:
-        # Case when checkpoint_path is provided but not the experiment name
-        if cfg.experiment.resume_checkpoint_path is not None:
-            cfg.experiment.name = cfg.experiment.resume_checkpoint_path.split("/")[-2]
-            logger.warning(f"experiment.name not set, infering {cfg.experiment.name} from resume_checkpoint_path.")
-        # Case when neither resume_run_id nor resume_checkpoint_path is provided. Generate a random name.
-        else:
-            cfg.experiment.name = f"{cfg.dataset.subconfig}-{str(torch.randint(9999, (1,)).item()).zfill(4)}"
-            if not cfg.experiment.offline_run:
-                wandb_entity = os.environ.get("WANDB_ENTITY")
-                api = wandb.Api()
-                runs = api.runs(f"{wandb_entity}/{cfg.experiment.group}")
-                while any(run.name == cfg.experiment.name for run in runs):
-                    cfg.experiment.name = f"{cfg.dataset.subconfig}-{str(torch.randint(9999, (1,)).item()).zfill(4)}"
-            logger.warning(f"experiment.name not set, generated random name {cfg.experiment.name}")
-
-    missing_keys: set[str] = OmegaConf.missing_keys(cfg)
-    if missing_keys:
-        raise RuntimeError(f"Missing keys in config: \n {missing_keys}")
-    if cfg.data_preprocessing.join_utts not in ["dynamic", "static", None, "None"]:
-        raise RuntimeError(f"Invalid value for join_utts: {cfg.data_preprocessing.join_utts}. Must be one of 'dynamic', 'static', or None.")
-    if cfg.data_preprocessing.join_utts == "None":
-        cfg.data_preprocessing.join_utts = None
-    if cfg.data_preprocessing.subsample_type not in ["examples", "words", "tokens", None]:
-        raise RuntimeError(f"Invalid value for subsample_type: {cfg.data_preprocessing.subsample_type}. Must be one of 'examples', 'words', or 'tokens'.")
-    if cfg.experiment.evaluate_babyslm and "English" not in cfg.dataset.subconfig:
-        raise RuntimeError("evaluate_babyslm is only supported for the English dataset.")
-
 def load_dataset(cfg : config.DatasetParams) -> datasets.Dataset:
-    """ Loads dataset from config """
+    """ Loads dataset from config
+
+    Args:
+        cfg (config.DatasetParams): hydra config object
+    Returns:
+        datasets.Dataset: loaded dataset
+    """
+    
     text_column = cfg.text_column
     load_columns = [text_column, "target_child_age"]
     features = datasets.Features({text_column: datasets.Value("string"), "target_child_age": datasets.Value("float")})
@@ -124,7 +66,12 @@ def load_dataset(cfg : config.DatasetParams) -> datasets.Dataset:
     return dataset
 
 def setup_wandb(cfg : config.ExperimentParams) -> None:
-    """ Setup wandb for logging """
+    """ Setup wandb for logging
+
+    Args:
+        cfg (config.ExperimentParams): hydra config object
+    """
+
     wandb_entity = os.environ["WANDB_ENTITY"]
     os.environ["WANDB_PROJECT"] = cfg.group
     wandb.config = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
@@ -141,7 +88,17 @@ def setup_wandb(cfg : config.ExperimentParams) -> None:
     )
 
 def subsample_dataset(dataset : datasets.Dataset, num_examples : int, subsample_type : Optional[str]) -> datasets.Dataset:
-    """ Subsample the dataset """
+    """ Subsamples the dataset based on the subsample_type
+    
+    Args:
+        dataset (datasets.Dataset): dataset to subsample
+        num_examples (int): number of examples to subsample
+        subsample_type (Optional[str]): type of subsampling to perform. Must be one of "examples", "words", or "tokens".
+
+    Returns:
+        datasets.Dataset: subsampled dataset
+    """
+
     if subsample_type is None or subsample_type == "examples":
         return dataset.shuffle().select(range(num_examples))
     elif subsample_type == "words":
@@ -162,3 +119,29 @@ def subsample_dataset(dataset : datasets.Dataset, num_examples : int, subsample_
         raise RuntimeError(f"Subsample count {num_examples} exceeds total token count in dataset")
     else:
         raise RuntimeError(f'Invalid value "{subsample_type}" for subsample_type. Must be one of "examples", "tokens" or "words".')
+    
+def load_tokenizer(cfg: config.TokenizerParams) -> Union[PreTrainedTokenizer, PreTrainedTokenizerFast]:
+    """
+    Sets up tokenizer for the model, based on tokenizer configurations
+
+    Args:
+        cfg (TransformerSegmentationConfig): hydra config object
+    """
+
+    full_tokenizer_name = cfg.name
+
+    # anything that's not name and vocab_size is an optional tokenizer kwarg
+    remove_keys = ["name", "vocab_size"]
+    tokenizer_kwargs = {
+        key: val
+        for key, val in cfg.items()
+        if key not in remove_keys and val is not None
+    }
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        full_tokenizer_name,
+        **tokenizer_kwargs,
+        use_auth_token=os.environ["HF_READ_TOKEN"],
+    )
+
+    return tokenizer
--- train.py
+++ train.py
@@ -21,7 +21,6 @@ from src.config import TransformerSegmentationConfig
 from src.datacollator import CustomDataCollatorForLanguageModeling
 from src.models import load_model
 from src.preprocessing import DataPreprocessor
-from src.tokenizer import load_tokenizer
 from src.trainer import CustomTrainer
 from src.utils import setup
 
@@ -36,11 +35,83 @@ DRY_RUN_SUBSAMPLE_FACTOR = 10 // (10 if torch.cuda.device_count() > 1 else 1)
 DRY_RUN_TRAIN_STEPS = 100
 DRY_RUN_WARMUP_STEPS = 10
 
+def check_and_set_environment_variables(cfg: TransformerSegmentationConfig) -> None:
+    """ Checks huggingface tokens exist and sets up wandb environment variables """
+
+    assert (
+        "HF_READ_TOKEN" in os.environ and "HF_WRITE_TOKEN" in os.environ
+    ), "HF_READ_TOKEN and HF_WRITE_TOKEN need to be set as environment variables. Check .env file and source it if necessary."
+
+    if cfg.experiment.offline_run:
+        os.environ["WANDB_DISABLED"] = "true"
+        os.environ["WANDB_MODE"] = "disabled"
+        if cfg.experiment.resume_run_id is not None:
+            raise RuntimeError("resume_run_id is set but offline_run is True. Ignoring resume_run_id.")
+    else:
+        assert (
+            "WANDB_ENTITY" in os.environ
+        ), "WANDB_ENTITY needs to be set as an environment variable if not running in offline mode. Check .env file and source it if necessary."
+        if cfg.experiment.resume_checkpoint_path and cfg.experiment.resume_run_id is None:
+            raise RuntimeError("resume_run_id must be set if resume_checkpoint_path is set")
+
+     # Disable parallelism in tokenizers to avoid issues with multiprocessing
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+def check_config(cfg: TransformerSegmentationConfig) -> None:
+    """ Infer missing config values (checkpoint path and/or experiment name) and check if keys are missing """
+
+    # It is possible to infer the name if resume_run_id is set or if resume_checkpoint_path is set. Otherwise, a random name is generated.
+    if cfg.experiment.resume_run_id:
+        # Case when resume_run_id provided but not the experiment name
+        wandb_entity = os.environ.get("WANDB_ENTITY")
+        if "name" not in cfg.experiment:
+            api = wandb.Api()
+            run = api.run(f"{wandb_entity}/{cfg.experiment.group}/{cfg.experiment.resume_run_id}")
+            cfg.experiment.name = run.name
+            logger.info(f"experiment.name not set, loaded {cfg.experiment.name} from resume_run_id {cfg.experiment.resume_run_id} on wandb.")
+        # Case when resume_run_id provided but not the checkpoint path
+        if not cfg.experiment.resume_checkpoint_path:
+            checkpoint_paths = [dir for dir in os.listdir(f"checkpoints/{cfg.experiment.group}/{cfg.experiment.name}") if dir.startswith("checkpoint")]
+            if len(checkpoint_paths) > 0:
+                checkpoint_numbers = [int(path.split("-")[-1]) for path in checkpoint_paths]
+                checkpoint_numbers.sort()
+                cfg.experiment.resume_checkpoint_path = f"checkpoints/{cfg.experiment.group}/{cfg.experiment.name}/checkpoint-{checkpoint_numbers[-1]}"
+                logger.info(f"resume_checkpoint_path not set, loaded {cfg.experiment.resume_checkpoint_path} from latest checkpoint.")
+            else:
+                raise RuntimeError(f"resume_run_id set but no checkpoints found in the run directory checkpoints/{cfg.experiment.group}/{cfg.experiment.name}. Please specify resume_checkpoint_path.")
+    if "name" not in cfg.experiment:
+        # Case when checkpoint_path is provided but not the experiment name
+        if cfg.experiment.resume_checkpoint_path is not None:
+            cfg.experiment.name = cfg.experiment.resume_checkpoint_path.split("/")[-2]
+            logger.warning(f"experiment.name not set, infering {cfg.experiment.name} from resume_checkpoint_path.")
+        # Case when neither resume_run_id nor resume_checkpoint_path is provided. Generate a random name.
+        else:
+            cfg.experiment.name = f"{cfg.dataset.subconfig}-{str(torch.randint(9999, (1,)).item()).zfill(4)}"
+            if not cfg.experiment.offline_run:
+                wandb_entity = os.environ.get("WANDB_ENTITY")
+                api = wandb.Api()
+                runs = api.runs(f"{wandb_entity}/{cfg.experiment.group}")
+                while any(run.name == cfg.experiment.name for run in runs):
+                    cfg.experiment.name = f"{cfg.dataset.subconfig}-{str(torch.randint(9999, (1,)).item()).zfill(4)}"
+            logger.warning(f"experiment.name not set, generated random name {cfg.experiment.name}")
+
+    missing_keys: set[str] = OmegaConf.missing_keys(cfg)
+    if missing_keys:
+        raise RuntimeError(f"Missing keys in config: \n {missing_keys}")
+    if cfg.data_preprocessing.join_utts not in ["dynamic", "static", None, "None"]:
+        raise RuntimeError(f"Invalid value for join_utts: {cfg.data_preprocessing.join_utts}. Must be one of 'dynamic', 'static', or None.")
+    if cfg.data_preprocessing.join_utts == "None":
+        cfg.data_preprocessing.join_utts = None
+    if cfg.data_preprocessing.subsample_type not in ["examples", "words", "tokens", None]:
+        raise RuntimeError(f"Invalid value for subsample_type: {cfg.data_preprocessing.subsample_type}. Must be one of 'examples', 'words', or 'tokens'.")
+    if cfg.experiment.evaluate_babyslm and "English" not in cfg.dataset.subconfig:
+        raise RuntimeError("evaluate_babyslm is only supported for the English dataset.")
+
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: TransformerSegmentationConfig):
 
-    setup.check_and_set_environment_variables(cfg)
-    setup.check_config(cfg)
+    check_and_set_environment_variables(cfg)
+    check_config(cfg)
     setup.set_seed(cfg.experiment.seed)
     if cfg.experiment.dry_run:
         logger.info("Running in dry run mode -- overriding config with values: ")
@@ -59,7 +130,7 @@ def main(cfg: TransformerSegmentationConfig):
         dataset['valid'] = dataset['valid'].select(range(0, dataset['valid'].num_rows, DRY_RUN_SUBSAMPLE_FACTOR))
 
     logger.info("Loading tokenizer")
-    tokenizer = load_tokenizer(cfg, dataset)
+    tokenizer = setup.load_tokenizer(cfg.tokenizer)
 
     if cfg.data_preprocessing.join_utts == "dynamic":
         # Set up custom data collator which joins examples to fill the context size
